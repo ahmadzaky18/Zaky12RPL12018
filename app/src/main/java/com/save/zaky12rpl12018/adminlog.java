28d7e3fafce6463849ed6312b770ccacd8466a58
ahmadzaky <ahmadzakyashshiddieqy@gmail.com>
2020-08-11 05:50:45 +0700

    add login

---
+++ app/src/main/java/com/save/zaky12rpl12018/adminlog.java
@@ -0,0 +1,24 @@
+package com.save.zaky12rpl12018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class adminlog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminlog);
+
+    }
+    public void login(View View){
+        startActivity(new Intent(this,beranda.class));
+    }
+    public void backcuslog(View View){
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
